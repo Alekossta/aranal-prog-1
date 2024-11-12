65ac6038db237de51eb6a324a2606c5b6849751b
Alekossta <alekosstavrinoudakis@gmail.com>
2024-11-12 17:17:02 +0200

    finding roots

--- main.m
+++ main.m
@@ -2,18 +2,29 @@ space_acuraccy = 0.001;
 eb_tolerance = 0.5 * 10^(-2);
 enr_tolerance = 0.5 * 10^(-6);
 
+%% f1
 % [-2,0]
 f1_1_left = -3;
 f1_1_right = 0;
-
-% [0,3]
-f1_2_left = 0;
-f1_2_right = 3;
 
 r1_1 = bisect(@f1, f1_1_left, f1_1_right, eb_tolerance);
 disp(r1_1)
 r1_1 = nr(@f1, @df1, r1_1, enr_tolerance);
 disp(r1_1);
 
-%r1_2 = bisect(@f1, f1_2_left, f1_2_right, eb_tolerance);
-%disp(r1_2)
+% [0,3]
+f1_2_left = 0;
+f1_2_right = 3;
+
+r1_2 = bisect(@f1, f1_2_left, f1_2_right, eb_tolerance);
+disp(r1_2)
+r1_2 = nr(@f1, @df1, r1_2, enr_tolerance);
+disp(r1_2);
+
+%% f2
+f2_left = -1;
+f2_right = 2;
+r2 = bisect(@f2, f2_left, f2_right, eb_tolerance);
+disp(r2);
+r2 = nr(@f2, @df2, r2, enr_tolerance);
+disp(r2);
