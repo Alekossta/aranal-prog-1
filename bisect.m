2c2e13b55c503a9598748eb93ebc9d340e02d556
Alekossta <alekosstavrinoudakis@gmail.com>
2024-11-13 15:03:14 +0200

    now keeping track of iterations

--- bisect.m
+++ bisect.m
@@ -1,4 +1,5 @@
-function root = bisect(f, left, right, tolerance)
+function [root, iterations] = bisect(f, left, right, tolerance)
+    iterations = 1;
     middle = (left + right) / 2;
     while ~equalWithTolerance(f(middle), 0, tolerance)
         if haveRoots(f(left), f(middle))
@@ -7,6 +8,7 @@ function root = bisect(f, left, right, tolerance)
             left = middle;
         end
         middle = (left + right) / 2;
+        iterations = iterations + 1;
     end
     root = middle;
 end
--- nr.m
+++ nr.m
@@ -1,7 +1,9 @@
-function root = nr(f, df, estimation, tolerance)
+function [root, iterations] = nr(f, df, estimation, tolerance)
     x_0 = estimation;
+    iterations = 0;
     while ~equalWithTolerance(f(x_0), 0, tolerance)
         x_0 = x_0 - (f(x_0) / df(x_0));
+        iterations = iterations + 1;
     end
     root = x_0;
 end
